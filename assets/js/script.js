b3a7dffe49596cf65846f7e46362bb5204c77499
Stebbi <stefodd@yahoo.com>
2022-03-12 15:38:59 -0600

    time block looping mechanism

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 var date = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 var hourBlock = $(".hour");
-var now = parseInt(moment().format("H"));
+var time = parseInt(moment().format("H"));
 
 
 
@@ -14,3 +14,18 @@ $(".saveBtn").on("click", function() {
 })
 
 
+$(hourBlock).each(function(i, hour) {
+    // get id of each hour
+    var hourTimeEl = parseInt($(this).attr("id"));
+
+    // assign the div element in the same row as the corresponding hour block a class depending on where it falls in comparison to the current time
+    if (hourTimeEl === time) {
+        $(this).next().addClass("present")
+    }
+    else if (hourTimeEl > time) {
+        $(this).next().addClass("future")
+    }
+    else if (hourTimeEl < time) {
+        $(this).next().addClass("past")
+    }
+});
