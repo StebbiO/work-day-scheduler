fb66e3322ec89221d7b5b0d1faf92034c765ae0f
Stebbi <stefodd@yahoo.com>
2022-03-13 16:50:25 -0500

    added reset button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,11 @@
 var date = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 var hourBlock = $(".hour");
 var time = parseInt(moment().format("H"));
-console.log(time)
-
-if(time == 1){
-    for(let i=0;i<18;i++){
-        localStorage.setItem(i,"")
-    }
-}
 
 
 $(".saveBtn").on("click", function() {
     var userText = $(this).siblings(".col-8").children("textarea").val();
     var timeBlock = $(this).siblings(".col-2").attr("id");
-    console.log(userText, timeBlock);
     localStorage.setItem(timeBlock, userText);
 })
 
@@ -23,7 +15,6 @@ $(hourBlock).each(function(i, hour) {
     // get id of each hour
     var hourTimeEl = parseInt($(this).attr("id"));
     var userEntry = localStorage.getItem(hourTimeEl)
-    console.log(userEntry);
     $(this).next().children("textarea").val(userEntry);
     // assign the div element in the same row as the corresponding hour block a class depending on where it falls in comparison to the current time
     if (hourTimeEl === time) {
@@ -44,4 +35,9 @@ $(".col-8").on("click", "textarea", function() {
 
 $(".col-8").on("blur", "textarea", function() {
     $(this).removeClass("form-control");
+})
+
+$(".btn-danger").on("click", function() {
+    localStorage.clear();
+    $("textarea").val("");
 })
