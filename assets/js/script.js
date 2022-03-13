59d406503ebda96fca7ff5533c54055669f76df7
Stebbi <stefodd@yahoo.com>
2022-03-13 14:32:03 -0500

    save feature added, lock icons resized

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,29 @@
 var date = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 var hourBlock = $(".hour");
 var time = parseInt(moment().format("H"));
+console.log(time)
 
-
-
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(textInput));
+if(time == 1){
+    for(let i=0;i<18;i++){
+        localStorage.setItem(i,"")
+    }
 }
+
 
 $(".saveBtn").on("click", function() {
     var userText = $(this).siblings(".col-8").children("textarea").val();
     var timeBlock = $(this).siblings(".col-2").attr("id");
     console.log(userText, timeBlock);
-    
+    localStorage.setItem(timeBlock, userText);
 })
 
 
 $(hourBlock).each(function(i, hour) {
     // get id of each hour
     var hourTimeEl = parseInt($(this).attr("id"));
-
+    var userEntry = localStorage.getItem(hourTimeEl)
+    console.log(userEntry);
+    $(this).next().children("textarea").val(userEntry);
     // assign the div element in the same row as the corresponding hour block a class depending on where it falls in comparison to the current time
     if (hourTimeEl === time) {
         $(this).next().addClass("present")
